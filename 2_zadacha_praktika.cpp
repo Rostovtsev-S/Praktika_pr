69c6d1af862913aed0c5972aac469d168e6bae7d
Rostovtsev-S <87003852+Rostovtsev-S@users.noreply.github.com>
2021-07-09 14:05:35 +0500

    Add files via upload

---
+++ 2_zadacha_praktika.cpp
@@ -0,0 +1,27 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+int main() {
+    
+   ifstream file("massiv.txt");
+   if (!file.is_open()) {
+       cout << "Error! File is not found!";
+   }
+   else  {
+       vector<int> vec;
+       copy(
+           istream_iterator<int>(file),
+           istream_iterator<int>(),
+           back_inserter(vec)); 
+       int _min = *min_element(vec.begin(), vec.end());
+
+       cout << "Min: " << _min << endl;
+
+       file.close();
+   
+    }
+    return 0;
+}
